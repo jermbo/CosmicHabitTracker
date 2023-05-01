84d7fcbdb126ccbefb7d3e6435c18e35d8827839
jermbo <jermbo@users.noreply.github.com>
2023-05-01 08:28:28 -0400

    foundational stuff

--- habit-vue/src/views/WelcomeScreen.vue
+++ habit-vue/src/views/WelcomeScreen.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-  </div>
+	<div>
+		<h1 class="heading-xl">Cosmic Habits</h1>
+		<h2 class="subheading-lg">Welcome to your habit tracker</h2>
+		<p class="body-lg">Start tracking your habits today and improve your life!</p>
+	</div>
 </template>
 
 <script setup lang="ts"></script>
