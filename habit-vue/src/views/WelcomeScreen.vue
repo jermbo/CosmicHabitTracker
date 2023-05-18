13e103f582dd86aac85c637febb0edf002695443
jermbo <jermbo@users.noreply.github.com>
2023-05-18 08:42:19 -0400

    Styles

--- habit-vue/src/views/WelcomeScreen.vue
+++ habit-vue/src/views/WelcomeScreen.vue
@@ -1,19 +1,5 @@
 <template>
-	<div>
-		<h1 class="heading-xl">Cosmic Habits</h1>
-		<h2 class="subheading-lg">Welcome to your habit tracker</h2>
-		<p class="body-lg">Start tracking your habits today and improve your life!</p>
-	</div>
-
-	<p class="body-lg">
-		Welcome to our Habit Tracker app. Our app is designed to help you build better habits by
-		allowing you to track your progress and stay motivated. We take your privacy seriously, which is
-		why all your data stays on your personal device. You can trust that your information is safe and
-		secure with us. Start your journey towards better habits today with our Habit Tracker app.
-	</p>
-
-	<!-- link to habits page -->
-	<RouterLink to="/habit">Start Tracking Habit</RouterLink>
+	<div class="app-container"></div>
 </template>
 
 <script setup lang="ts"></script>
