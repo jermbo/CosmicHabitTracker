fe31517a67416586952c7306d1a9dce0c394cee1
jermbo <jermbo@users.noreply.github.com>
2023-09-23 11:39:33 -0400

    Restarting vue

--- habit-vue/src/App.vue
+++ habit-vue/src/App.vue
@@ -1,34 +1,11 @@
+<script setup lang="ts"></script>
+
 <template>
-	<AppHeader />
-	<div class="app-content">
-		<NavBar />
-		<main class="projects-section">
-			<div class="projects-section-header">
-				<h2>Welcome User!</h2>
-				<p class="time">{{ theDate }}</p>
-			</div>
-
-			<section class="projects-section-line">
-				<QuickStats />
-				<DisplayOptions />
-			</section>
-
-			<div class="project-boxes">
-				<div class="project-box-wrapper"></div>
-			</div>
-		</main>
-	</div>
+	<h1>What up</h1>
 </template>
 
-<script setup lang="ts">
-import AppHeader from '@/components/AppHeader.vue';
-import NavBar from '@/components/NavBar.vue';
-import QuickStats from '@/components/QuickStats.vue';
-import DisplayOptions from '@/components/DisplayOptions.vue';
-
-const today = new Date();
-const options: Intl.DateTimeFormatOptions = { weekday: 'long', month: 'long', day: 'numeric' };
-const theDate = today.toLocaleDateString('en-US', options);
-</script>
-
-<style scoped></style>
+<style scoped>
+h1 {
+	color: goldenrod;
+}
+</style>
