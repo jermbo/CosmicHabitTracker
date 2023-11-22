f642c07506d430d73ef4c51164c4e295b6559e21
jermbo <jermbo@users.noreply.github.com>
2023-11-22 13:01:13 -0500

    Testing data storage

--- habit-vue/src/stores/Habits.ts
+++ habit-vue/src/stores/Habits.ts
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import { HabitData } from 'src/helpers/interfaces';
 
+const randomNumber = () => Math.floor(Math.random() * 10) + 1;
 export const useHabitsStore = defineStore('habits', () => {
 	const habits = useLocalStorage('habits:data', [] as HabitData[]);
 
@@ -10,16 +11,17 @@ export const useHabitsStore = defineStore('habits', () => {
 		const currentDate = new Date();
 		const randomHabit: HabitData = {
 			date: currentDate.toISOString().split('T')[0],
-			mood: 4,
-			energy: 5,
-			meditationMinutes: 5,
-			glassesOfWater: 5,
-			cupsOfCoffee: 5,
+			mood: randomNumber(),
+			energy: randomNumber(),
+			meditationMinutes: randomNumber(),
+			glassesOfWater: randomNumber(),
+			cupsOfCoffee: randomNumber(),
 			weight: 5,
 			gratefulFor: 'Family',
 			timestamp: currentDate.getTime()
 		};
 		habits.value.push(randomHabit);
 	}
+
 	return { habits, addHabit };
 });
--- habit-vue/src/views/Habits.vue
+++ habit-vue/src/views/Habits.vue
@@ -2,6 +2,12 @@
 	<div class="habits-wrapper">
 		<DatePicker />
 		<button @click="addHabit">Add Habit</button>
+
+		<template v-if="habits?.length > 0">
+			<div v-for="habit in habits" :key="habit.timestamp">
+				{{ habit.energy }} - {{ habit.timestamp }} - {{ habit.mood }}
+			</div>
+		</template>
 	</div>
 </template>
 
@@ -10,7 +16,6 @@ import DatePicker from '@components/DatePicker/DatePicker.vue';
 import { useHabitsStore } from '@stores/Habits';
 
 const { habits, addHabit } = useHabitsStore();
-console.log(habits);
 </script>
 
 <style scoped></style>
