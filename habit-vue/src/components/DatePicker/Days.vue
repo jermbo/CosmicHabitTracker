679a94941276f870003ddca166af88bfa457a7d1
jermbo <jermbo@users.noreply.github.com>
2023-11-25 18:14:05 -0500

    Updated from button to radio to provide a better keyboard experience

--- habit-vue/src/components/DatePicker/Days.vue
+++ habit-vue/src/components/DatePicker/Days.vue
@@ -1,17 +1,22 @@
 <template>
 	<div class="days-wrapper">
 		<template v-for="date in datesInMonth" :key="date.day">
-			<button
-				class="day"
-				:class="`${activeDate === date.day ? 'active' : ''}`"
-				@click="setDate(date)"
-			>
-				<span class="date">
-					<span>{{ date.weekday }}</span>
-					<span>{{ date.day }}</span>
-				</span>
-				<span class="mood">😐</span>
-			</button>
+			<div class="day">
+				<input
+					type="radio"
+					name="day"
+					@input="setDate(date)"
+					:id="`${date.day}`"
+					:checked="activeDate === date.day"
+				/>
+				<label :for="`${date.day}`" :class="`${activeDate === date.day ? 'active' : ''}`">
+					<span class="date">
+						<span>{{ date.weekday }}</span>
+						<span>{{ date.day }}</span>
+					</span>
+					<span class="mood">😐</span>
+				</label>
+			</div>
 		</template>
 	</div>
 </template>
@@ -39,6 +44,21 @@ onMounted(() => {
 }
 
 .day {
+	position: relative;
+}
+
+input[type='radio'] {
+	position: absolute;
+	z-index: 0;
+	top: 50%;
+	left: 50%;
+
+	&:focus-visible + label {
+		box-shadow: var(--outline);
+	}
+}
+
+label {
 	border-radius: var(--round-md);
 	display: flex;
 	flex-direction: column;
@@ -47,24 +67,28 @@ onMounted(() => {
 	border: 0;
 	padding: 0;
 	transition: var(--transition);
-
-	&:focus-visible,
-	&:hover {
-		box-shadow: var(--outline);
-	}
+	z-index: 10;
+	position: relative;
 }
 
+label:hover,
 .active {
-	box-shadow: var(--outline);
+	scale: 1.1;
+
+	.date {
+		filter: invert(1);
+		opacity: 0.75;
+	}
 }
 
 .date {
 	border-radius: var(--round-md);
-	display: inline-block;
 	padding: var(--space-md);
 	background: var(--bkg);
 	display: flex;
 	flex-direction: column;
+	min-width: 60px;
+	text-align: center;
 
 	& span:nth-child(2) {
 		font-weight: bold;
